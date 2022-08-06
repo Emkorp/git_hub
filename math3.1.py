df623b4e5853beb536450982c6b6daa60e5d7644
Emkorp <106599083+Emkorp@users.noreply.github.com>
2022-08-07 00:18:47 +0300

    Add files via upload

---
+++ math3.1.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+lst = [10, 10, 10]
+lst2 = [0, 0, -10]
+
+vctr = np.array(lst)
+vctr2 = np.array(lst2)
+
+print(vctr)
+print(vctr2)
+
+vctr_add = vctr + vctr2
+print(vctr_add)
+import math
+length = math.sqrt(10 * 10 + 10 * 10 + 0 * 0)
+print(length)
